7a67b588d23aff55284cb407cd273267f0fca0b0
hotansang123 <cucbong0@gmail.com>
2022-04-14 08:54:58 +0700

    push to git

---
+++ system/config.php
@@ -0,0 +1,14 @@
+<?php
+define('HOST_DB','ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('NAME_DB','koata9yd8tr3xd1j');
+define('USER_DB','hvzba24d77gkqn3z');
+define('PASS_DB','m3c21aexrr74y04e');
+define('ROOT_URL','/asmphp2');
+define('ADMIN_URL',ROOT_URL.'/admin');
+define('SITE_URL',ROOT_URL.'/site/');
+define('SYSTEM_PATH',ROOT_URL.'/system');
+define('PATH_IMG_ADMIN','../uploads/');
+define('PAGE_SIZE',3);
+define('PATH_IMG_SITE','../uploads/');
+// session_start();
+?>
